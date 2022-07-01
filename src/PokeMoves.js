3d8cd14981e08252a4c0bd9ea7399a1c92d64ffb
The-Flying-Dev <leandranm@outlook.com>
2022-06-30 23:08:49 -0700

    Complete Tasks

--- src/PokeMoves.js
+++ src/PokeMoves.js
@@ -1,9 +1,24 @@
+import { moves } from "./data";
+import PokeMoveCard from "./PokeMoveCard";
 
 
 function PokeMoves() {
+
+  // (1) first map through array
+  // (2) return the {VALUES} inside each <li> element that is being rendered
+  // (3) React needs a UNIQUE key for each ITEM when mapping through an array
+
   return (
-    
-  )
+    <div>
+      <h1>PokeMoves</h1>
+      <ul>        
+        {moves.map(item => ( 
+          //<li key={item.id}>{item.move}</li>
+          <PokeMoveCard key={item.id} {...item} />        
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default PokeMoves;
